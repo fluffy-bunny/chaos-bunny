09afe90ff140341002b11f35291e9ea817c4b091
Herb Stahl <ghstahl@gmail.com>
2020-08-10 10:44:28 -0700

    Docker builds

---
+++ src/run-docker-compose.ps1
@@ -0,0 +1,11 @@
+echo $env:ApplicationInsights_InstrumentationKey 
+echo $env:oidc_1_clientId 
+echo $env:oidc_1_clientSecret 
+echo $env:oidc_1_authority 
+echo $env:oidc_2_clientId 
+echo $env:oidc_2_clientSecret 
+echo $env:oidc_2_authority 
+docker-compose -f docker-compose.yml up
+
+
+
