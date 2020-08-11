96800eb1475eb88cfbe481f4b11f94f3b172aad8
Herb Stahl <ghstahl@gmail.com>
2020-08-11 13:47:48 -0700

    print out request

--- src/acr-push.ps1
+++ src/acr-push.ps1
@@ -1,5 +1,5 @@
 docker images
 az acr login --name acrchaosbunny
-docker tag chaosbunny/app:latest acrchaosbunny.azurecr.io/chaosbunny/app
+docker tag chaosbunny/app:latest acrchaosbunny.azurecr.io/chaosbunny/app:latest
 
-docker push acrchaosbunny.azurecr.io/chaosbunny/app
+docker push acrchaosbunny.azurecr.io/chaosbunny/app:latest
